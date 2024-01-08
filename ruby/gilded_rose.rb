79c0367dc7338ae57d044020337d07e3af56956f
NensiMakawana <nensi.makawana@gmail.com>
2024-01-08 21:15:36 +0530

    Refactor the code

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -17,44 +17,50 @@ class GildedRose
           end
         end
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
+        calculate_quality_for_less_than_50(item)
       end
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
       if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-                if item.name == "Conjured Mana Cake"
-                  item.quality = item.quality - 1
-                end
-              end
+        calculate_quality_after_sell(item)
+      end
+    end
+  end
+  
+  private
+
+  def calculate_quality_for_less_than_50(item)
+    if item.quality < 50
+      item.quality = item.quality + 1
+      if item.name == "Backstage passes to a TAFKAL80ETC concert"
+        if item.sell_in < 11 and item.quality < 50
+          item.quality = item.quality + 1
+        end
+        if item.sell_in < 6 and item.quality < 50
+          item.quality = item.quality + 1
+        end
+      end
+    end
+  end
+
+  def calculate_quality_after_sell(item)
+    if item.name != "Aged Brie"
+      if item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
+            if item.name == "Conjured Mana Cake"
+              item.quality = item.quality - 1
             end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
           end
         end
+      else
+        item.quality = item.quality - item.quality
+      end
+    else
+      if item.quality < 50
+        item.quality = item.quality + 1
       end
     end
   end
